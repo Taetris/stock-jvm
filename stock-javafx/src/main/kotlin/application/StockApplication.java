f293359672ef8106dea9b12512b6aea9152ac0ed
Ahmed Arnaut <ahmed.arnaut@hotmail.com>
2018-08-04 23:02:04 +0200

    Removed controller.

--- stock-javafx/src/main/kotlin/application/StockApplication.java
+++ stock-javafx/src/main/kotlin/application/StockApplication.java
@@ -1,9 +1,13 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StockApplication extends Application {
 
@@ -12,8 +16,9 @@ public class StockApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Scene scene = new Scene(new VBox(), 640, 480);
+    public void start(Stage primaryStage) throws IOException {
+        Parent main = FXMLLoader.load(StockApplication.class.getResource("../../resources/stock-overview.fxml"));
+        Scene scene = new Scene(main, 640, 480);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
