2a7ca09616614fb1df79f40545c27cf9a03101c3
Ahmed Arnaut <ahmed.arnaut@hotmail.com>
2018-08-05 11:26:38 +0200

    Implemented mock customer repository.

--- stock-storage/src/main/kotlin/repository/customer/CustomerRepository.kt
+++ stock-storage/src/main/kotlin/repository/customer/CustomerRepository.kt
@@ -19,30 +19,41 @@ interface CustomerRepository {
     /**
      * Removes an existing customer from the repository.
      *
-     * @param id: id of the customer to remove.
+     * @param customer [Customer]: customer to remove.
      * @throws RepositoryException in case deletion has failed.
      */
     @Throws(RepositoryException::class)
-    fun removeCustomer(id: Int)
+    fun removeCustomer(customer: Customer)
 
     /**
-     * Updates an existing customer with new values.
+     * Updates an existing customer with new values. Customer associated with the id of the
+     * new customer will be removed and replaced with the new customer.
      *
-     * @param customer [Customer] updated customer that should replace the existing one.
+     * @param customer [Customer]: updated customer that should replace the existing one.
      * @throws RepositoryException in case update has failed.
      */
     @Throws(RepositoryException::class)
     fun updateCustomer(customer: Customer)
 
     /**
+     * Checks if there is a customer stored, associated with the given id.
+     *
+     * @param id: id of the customer
+     * @return [Boolean]: true if there's a customer associated with the given id, otherwise false.
+     */
+    @Throws(RepositoryException::class)
+    fun containsCustomer(id: Int): Boolean
+
+    /**
      * Gets the customer with assigned id.
      *
      * @param id: id of the customer to retrieve.
      * @return [Customer] the retrieved customer.
-     * @throws RepositoryException in case retrieval has failed.
+     * @throws RepositoryException in case there was no customer associated with the given id or there
+     * was another issue with the retrieval.
      */
     @Throws(RepositoryException::class)
-    fun getCustomer(id: Int): Customer
+    fun getCustomerById(id: Int): Customer
 
     /**
      * Gets all customers from the repository.
---
+++ stock-storage/src/main/kotlin/repository/customer/mock/CustomerRepositoryMock.kt
@@ -0,0 +1,69 @@
+package repository.customer.mock
+
+import org.slf4j.LoggerFactory
+import repository.RepositoryException
+import repository.customer.Customer
+import repository.customer.CustomerRepository
+
+/**
+ * Mock implementation of the [CustomerRepository] that uses an in-memory storage.
+ */
+class CustomerRepositoryMock : CustomerRepository {
+
+    private val customers: MutableList<Customer> = ArrayList()
+    private val logger = LoggerFactory.getLogger(CustomerRepositoryMock::class.java)
+
+    override fun insertCustomer(customer: Customer) {
+        if (!containsCustomer(customer.id)) {
+            customers.add(customer)
+            logger.info("Inserted customer: '{}'", customer)
+        } else {
+            throw RepositoryException("Failed to insert customer '$customer'.")
+        }
+    }
+
+    override fun removeCustomer(customer: Customer) {
+        if (containsCustomer(customer.id)) {
+            customers.remove(customer)
+            logger.info("Removed customer '{}'", customer)
+        } else {
+            throw RepositoryException("Failed to remove customer '$customer'.")
+        }
+    }
+
+    override fun updateCustomer(customer: Customer) {
+        if (containsCustomer(customer.id)) {
+            customers.remove(customer)
+            customers.add(customer)
+            logger.info("Updated customer: '{}'", customer)
+        } else {
+            throw RepositoryException("Failed to update customer with the id '${customer.id}'.")
+        }
+    }
+
+    override fun containsCustomer(id: Int): Boolean {
+        var result = false
+        customers.forEach { customer ->
+            if (customer.id == id) {
+                result = true
+            }
+        }
+
+        logger.info("Contains customer with id '$id': $result")
+        return result
+    }
+
+    override fun getCustomerById(id: Int): Customer {
+        customers.forEach { customer ->
+            if (customer.id == id) {
+                return customer
+            }
+        }
+
+        throw RepositoryException("No customer found associated with the id '$id'.")
+    }
+
+    override fun getAllCustomers(): List<Customer> {
+        return customers
+    }
+}
